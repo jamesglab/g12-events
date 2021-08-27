64ac55247d1ad6f7a61eabe63c71d579c00b59ef
Miguel torres <lt@wap.us>
2021-08-27 09:50:28 -0500

    compile dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   // apiUrlDonations: "https://gateway.mcidonaciones.com:1100/",
   // apiUrlConexion12: "https://plataforma.mci12.com:444/",
   apiUrlG12Connect: {
-    donations: 'https://813b-201-184-17-202.ngrok.io/api/v2/',
+    donations: 'https://api.g12connect.com/api/v2/',
     managment: 'https://api.g12connect.com/api/v2/',
     users: 'https://api.g12connect.com/api/v2/',
     payments: 'https://api.g12connect.com/api/v2/'
