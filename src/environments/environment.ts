c01affe192a20100be8f1ceed5b681f1a4d80d32
Miguel torres <lt@wap.us>
2022-01-17 13:53:15 -0500

    enviroments api dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
   production: false,
   apiUrlG12Connect: {
-    donations: 'https://api.g12connect.com/api/v2/donations',
-    managment: 'https://api.g12connect.com/api/v2/managment',
-    users: 'https://api.g12connect.com/api/v2/users',
-    payments: 'https://api.g12connect.com/api/v2/payments',
-    payments_v3: 'https://api.g12connect.com/api/v3/payments',
+    donations: 'https://dev.api.g12connect.com/api/v2/donations',
+    managment: 'https://dev.api.g12connect.com/api/v2/managment',
+    users: 'https://dev.api.g12connect.com/api/v2/users',
+    payments: 'https://dev.api.g12connect.com/api/v2/payments',
+    payments_v3: 'https://dev.api.g12connect.com/api/v3/payments',
   },
   urlResponse: 'https://dev.eventosg12.com/payment/transaction',
 };
